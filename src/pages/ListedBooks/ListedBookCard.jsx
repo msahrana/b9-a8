a369ac685ff245aa9fe35754fefb3f13d6a3d2b5
msahrana <msahrana@gmail.com>
2024-03-27 00:29:42 +0600

    listed book card item is added

--- src/pages/ListedBooks/ListedBookCard.jsx
+++ src/pages/ListedBooks/ListedBookCard.jsx
@@ -1,3 +1,8 @@
+import {CiLocationOn} from "react-icons/ci";
+import {Link} from "react-router-dom";
+import {FaUserGroup} from "react-icons/fa6";
+import {LiaPagerSolid} from "react-icons/lia";
+
 const ListedBookCard = ({listedBook}) => {
   const {
     image,
@@ -9,17 +14,18 @@ const ListedBookCard = ({listedBook}) => {
     tags,
     publisher,
     year_of_publishing,
+    id,
   } = listedBook;
 
   return (
-    <div className="container mx-auto w-2/3 flex flex-col lg:flex-row gap-10 border border-gray-200 rounded-md mb-4">
-      <div className="px-10 pt-10">
-        <img src={image} alt="Shoes" className="rounded-xl" />
+    <div className="container mx-auto p-3 w-2/3 flex flex-col lg:flex-row gap-10 border border-gray-200 rounded-md mb-4">
+      <div className="px-10 py-4 bg-gray-200 rounded-lg">
+        <img src={image} alt="Shoes" className="rounded-xl w-32" />
       </div>
-      <div className="">
+      <div className="space-y-2">
         <h2 className="card-title">{book_name}</h2>
         <p>By : {author}</p>
-        <div>
+        <div className="flex space-x-5 items-center">
           <h3>Tags : </h3>
           <p className="text-[#23be0a] bg-[#23be0a0d] rounded-full px-4 py-1 text-base font-semibold">
             {tags?.[0]}
@@ -27,10 +33,29 @@ const ListedBookCard = ({listedBook}) => {
           <p className="text-[#23be0a] bg-[#23be0a0d] rounded-full px-4 py-1 text-base font-semibold">
             {tags?.[1]}
           </p>
+          <CiLocationOn />
+          <p>Published : {year_of_publishing}</p>
         </div>
-        <div></div>
+        <div className="flex space-x-5 items-center">
+          <FaUserGroup />
+          <p>Publisher : {publisher}</p>
+          <LiaPagerSolid />
+          <p>Pages: {total_pages}</p>
+        </div>
         <div className="divider"></div>
-        <div></div>
+        <div className="flex space-x-5 items-center">
+          <div className="bg-[#328eff26] px-2 py-1 rounded-full">
+            <p className="text-[#328EFF]">Category: {category}</p>
+          </div>
+          <div className="bg-[#ffac3326] px-2 py-1 rounded-full">
+            <p className="text-[#ffac33]">Rating: {rating}</p>
+          </div>
+          <Link to={`/books/${id}`}>
+            <button className="bg-[#23be0a] px-2 py-1 rounded-full text-white">
+              View Details
+            </button>
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/utility/localStorage.js
+++ src/utility/localStorage.js
@@ -1,18 +1,22 @@
+import swal from "sweetalert";
+
 export const getFromLocalStorage = () => {
-  const book = JSON.parse(localStorage.getItem("listed")) || [];
+  const book = JSON.parse(localStorage.getItem("read")) || [];
   return book;
 };
 
 export const saveToLocalStorage = (book) => {
   console.log(book);
-  const saveBook = JSON.parse(localStorage.getItem("listed")) || [];
+  const saveBook = JSON.parse(localStorage.getItem("read")) || [];
   console.log(saveBook);
   const isExist = saveBook.find((item) => item.id == book.id);
   if (!isExist) {
     saveBook.push(book);
-    localStorage.setItem("listed", JSON.stringify(saveBook));
-    alert("Added successfully to localStorage");
+    localStorage.setItem("read", JSON.stringify(saveBook));
+    swal("Good job!", "You read this book completely!", "success");
+    return;
   } else {
-    alert("Already save this item");
+    swal("Sorry!", "Already uou read this type book!", "error");
+    return;
   }
 };
